165cfbe430782149603439a9e23e26a5a9e9b3c9
ldn <ldnjs@sina.com>
2020-03-09 15:21:55 +0800

    null

--- src/global.tsx
+++ src/global.tsx
@@ -55,7 +55,7 @@ if (pwa) {
       description: formatMessage({ id: 'app.pwa.serviceworker.updated.hint' }),
       btn,
       key,
-      onClose: async () => {},
+      onClose: async () => { },
     });
   });
 } else if ('serviceWorker' in navigator) {
